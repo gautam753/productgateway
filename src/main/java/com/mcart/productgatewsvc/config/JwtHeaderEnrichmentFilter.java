26d3f361b5168cba62af3f0ac7ec4dc4a8571cd2
goutampaul <goutampaul.prf@gmail.com>
2026-02-28 22:43:56 +0530

    Updating JwtHeaderEnrichmentFilter

--- src/main/java/com/mcart/productgatewsvc/config/JwtHeaderEnrichmentFilter.java
+++ src/main/java/com/mcart/productgatewsvc/config/JwtHeaderEnrichmentFilter.java
@@ -2,9 +2,11 @@ package com.mcart.productgatewsvc.config;
 
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
 
 @Component
@@ -15,14 +17,31 @@ public class JwtHeaderEnrichmentFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange,
                              org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
-
-        return exchange.getPrincipal()
+        /*return exchange.getPrincipal()
                 .filter(JwtAuthenticationToken.class::isInstance)
                 .cast(JwtAuthenticationToken.class)
                 .map(jwtAuth -> {
                     String userId = jwtAuth.getToken().getSubject();
                     return exchange.mutate()
                             .request(r -> r.header(INTERNAL_HEADER, userId))
+                            .build();
+                })
+                .defaultIfEmpty(exchange)
+                .flatMap(chain::filter);
+        */
+    	return exchange.getPrincipal()
+                .filter(JwtAuthenticationToken.class::isInstance)
+                .cast(JwtAuthenticationToken.class)
+                .map(jwtAuth -> {
+                    String userId = jwtAuth.getToken().getSubject();
+
+                    ServerHttpRequest mutatedRequest = exchange.getRequest()
+                            .mutate()
+                            .header(INTERNAL_HEADER, userId)
+                            .build();
+
+                    return exchange.mutate()
+                            .request(mutatedRequest)
                             .build();
                 })
                 .defaultIfEmpty(exchange)
