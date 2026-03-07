bd3117b15303ccb0e809adbfc42cfdf3d1178557
goutampaul <goutampaul.prf@gmail.com>
2026-03-07 16:39:44 +0530

    Adding cors confihuration

--- src/main/java/com/mcart/productgatewsvc/config/SecurityConfig.java
+++ src/main/java/com/mcart/productgatewsvc/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.mcart.productgatewsvc.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -22,4 +29,21 @@ public class SecurityConfig {
             .oauth2ResourceServer(oauth2 -> oauth2.jwt());
         return http.build();
     }
+    
+    @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("localhost:8060,*".split(",")));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setExposedHeaders(List.of("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
+	@Bean
+	CorsWebFilter corsFilter() {
+		return new CorsWebFilter(corsConfigurationSource());
+	}
 }
